aeb7f324cbe1fb7cc29ff2d1f94332d336cefc1f
hornbuck <124624417+hornbuck@users.noreply.github.com>
2024-11-17 21:34:59 -0800

    Hosting Repairs
    Added map render, helicopter sidebar, and fixed web hosting bug in index.html. Import phaser is commented out on purpose to test this.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+//import Phaser from 'phaser';
 import MainScene from './mainScene.js';
 
 // Log to confirm app.js is running
--- js/ui.js
+++ js/ui.js
@@ -29,12 +29,26 @@ export function createHUD(scene) {
         wordWrap: { width: 80 },
     }).setOrigin(0.5, 0.5);
 
+    // Group 3: Helicopter
+    const helicopterIcon = scene.add.image(750, sidebarCenter + iconSpacing + (iconSpacing/5), 'helicopter').setScale(0.8).setOrigin(0.5, 0.5);
+    const helicopterText = scene.add.text(750, sidebarCenter + (iconSpacing/5) + (iconTextOffset*3.7), 'Helicopters: 3/3', {
+        font: '12px Arial',
+        fill: '#ffffff',
+        align: 'center',
+        wordWrap: { width: 80 },
+    }).setOrigin(0.5, 0.5);
+
     console.log("Fire text lowered by extra offset");
 }
 
 export function preloadHUD(scene) {
     console.log("preloadHUD called");
-    scene.load.image('waterIcon', 'Assets/water.png');
+    scene.load.image('waterIcon', 'Assets/water-2.png');
     scene.load.image('fireIcon', 'Assets/fire_suppression.png');
+    scene.load.image('water', 'Assets/water.png');
+    scene.load.image('fire', 'Assets/fire.png');
+    scene.load.image('hotshot', 'Assets/hotshot.png');
+    scene.load.image('tanker', 'Assets/tanker.png');
+    scene.load.image('helicopter', 'Assets/helicopter.png');
 }
 
