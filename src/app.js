373a27081551c5318089ad53a49e8c60b25aed54
t_renn <rennt@oregonstate.edu>
2025-02-17 19:21:37 -0800

    Integrate IntroScene with the SignupScene and LoginScene

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import IntroScene from './scenes/IntroScene.js';
 import MainScene from './scenes/mainScene.js';
+import SignupScene from "./scenes/signupScene.js";
+import LoginScene from "./scenes/loginScene.js";
 
 // Log to confirm app.js is running
 console.log("Phaser Game Initializing...");
@@ -9,11 +11,15 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [IntroScene, MainScene],
+    parent: 'game-container',
+    scene: [IntroScene, SignupScene, LoginScene, MainScene],
     backgroundColor: '#282c34',
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    dom: {
+        createContainer: true,
     }
 };
 
