38a9a742de1b9f9f48fb39e66e03fee6844bf128
Cat Randquist <crandquist@gmail.com>
2025-02-17 17:40:55 -0800

    Add IntroScene to game configuration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
-import Phaser from "phaser";
 import MainScene from './scenes/mainScene.js';
-import LoginScene from "./scenes/loginScene.js";
-import SignupScene from "./scenes/signupScene.js";
 
 // Log to confirm app.js is running
 console.log("Phaser Game Initializing...");
@@ -10,16 +7,12 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    parent: 'game-container',
-    scene: [MainScene, LoginScene, SignupScene],
+    scene: [MainScene],
     backgroundColor: '#282c34',
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
     },
-    dom: {
-        createContainer: true,
-    }
 };
 
 // Create the game instance locally
