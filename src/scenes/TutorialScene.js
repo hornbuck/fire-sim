811ee29b20de9c3e0a52b8d0b279de868258e638
Cat Randquist <crandquist@gmail.com>
2025-05-10 19:11:22 -0700

    Add TutorialScene content creation and button functionality

--- src/scenes/TutorialScene.js
+++ src/scenes/TutorialScene.js
@@ -1,3 +1,5 @@
+import { createDrawnButton } from '../components/ButtonManager.js';
+
 export default class TutorialScene extends Phaser.Scene {
     constructor() {
         super({ key: 'TutorialScene' });
@@ -6,6 +8,7 @@ export default class TutorialScene extends Phaser.Scene {
 
     preload() {
         this.load.image('page 1', 'assets/tutorial icons/page-1.png');
+        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
     }
 
     create() {
@@ -14,6 +17,55 @@ export default class TutorialScene extends Phaser.Scene {
         const gameWidth = this.cameras.main.width;
         const gameHeight = this.cameras.main.height;
 
-        const menu = this.add.image(centerX, centerY, 'page 1').setOrigin(0.5);
+        // Load WebFont before creating text
+        if (typeof WebFont !== 'undefined') {
+            WebFont.load({
+                google: {
+                    families: ['Press Start 2P']
+                },
+                active: () => {
+                    this.createTutorialContent();
+                }
+            });
+        } else {
+            // If WebFont isn't available, still create the content
+            this.createTutorialContent();
+        }
+    }
+
+    createTutorialContent() {
+        const centerX = this.cameras.main.centerX;
+        const centerY = this.cameras.main.centerY;
+        const gameWidth = this.cameras.main.width;
+        const gameHeight = this.cameras.main.height;
+
+        // Add background
+        const background = this.add.rectangle(centerX, centerY, gameWidth, gameHeight, 0x2d3436).setAlpha(0.9);
+        
+        // Add title
+        const title = this.add.text(centerX, centerY - 200, 'Tutorial', {
+            fontFamily: '"Press Start 2P"',
+            fontSize: '32px',
+            color: '#ffffff',
+            align: 'center'
+        }).setOrigin(0.5);
+        
+        // Tutorial content - use the original image for content if needed
+        const tutorialContent = this.add.image(centerX, centerY, 'page 1').setOrigin(0.5);
+        
+        // Add back button
+        const backButton = createDrawnButton(this, {
+            x: centerX,
+            y: centerY + 200,
+            width: 250,
+            height: 50,
+            backgroundColor: 0x8B0000,
+            hoverColor: 0xA52A2A,
+            text: 'Back to Menu',
+            fontSize: '16px',
+            onClick: () => {
+                this.scene.start('MenuScene');
+            }
+        });
     }
 }
