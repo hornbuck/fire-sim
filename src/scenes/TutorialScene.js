25272ca32b29067391926f21be0c32e5f06f1787
hornbuck <hornbuck@oregonstate.edu>
2025-05-14 21:54:36 -0700

    Added shop instructions to the tutorial

--- src/scenes/TutorialScene.js
+++ src/scenes/TutorialScene.js
@@ -106,7 +106,14 @@ export default class TutorialScene extends Phaser.Scene {
             `Let's deploy some more assets. This fire is growing fast.`,
             `Try to deploy 3 helicopters.`,
             `Oh! Looks like there is a cooldown. Use fire extinguishers while you wait.`,
-            `Alternate between different resources to fight as many flames as you can.`
+            `Alternate between different resources to fight as many flames as you can.`,
+            `Uh oh! You ran out! Click the shop icon in the bottom right corner.`,
+            `Here, you can purchase more assets! Click any of the green plus signs.`,
+            `This controls the number of assets you want to buy.`,
+            `Believe it or not, you actually earned some coins already!`,
+            `Every time you put out a fire, you earn coins.`,
+            `When you're ready, click Purchase.`,
+            `Let's go fight more fires!`
         ]
 
         let marker = this.add.rectangle(centerX, centerY, gameWidth - 120, gameHeight - 70, '0x9ff86f', 0)
@@ -126,13 +133,15 @@ export default class TutorialScene extends Phaser.Scene {
         }).setOrigin(0.5).setX(centerX).setY(centerY);
 
         let d_count = 0;
+        let toggle = false;
         marker.alwaysEnabled = false
         
         // Roll through all dialog to aid the player in how to play the game
         this.input.on('pointerdown', (pointer) => {
             if (marker.getBounds().contains(pointer.x, pointer.y)) {   // only the game area is clickable
+                console.log(`D_COUNT: ${d_count}`);
                 text.setText(t_storage[d_count]);
-                d_count += 1
+                if (toggle === false) d_count += 1;
 
                 // Pause the game when explaining fuel and flammability
                 if (d_count >= 13 && d_count <= 18) {
@@ -141,11 +150,12 @@ export default class TutorialScene extends Phaser.Scene {
                     this.scene.resume('MapScene');
                 }
 
-                // Give the player some time to play with the assets until they run out
-                if (d_count >= 22) {
+                // Give the player some time to play with the assets until they run out OR buying assets from the shop
+                if (d_count === 22 || d_count == 28) {
                     dialog.setVisible(false);
                     text.setVisible(false);
-                    d_count -= 1
+                    d_count -= 1;
+                    toggle = true;
                 }
 
                 // Reset location of talking firefighter when the game begins
@@ -174,8 +184,14 @@ export default class TutorialScene extends Phaser.Scene {
                 }
 
                 // Pause the game when the player runs out of an asset
-                if (hose === 0 || extinguisher === 0 || helicopter === 0 || firetruck === 0 || airtanker === 0 || hotshotcrew === 0 || smokejumper === 0) {
-                    this.scene.pause('MapScene');
+                if (d_count < 27) {
+                    if (hose === 0 || extinguisher === 0 || helicopter === 0 || firetruck === 0 || airtanker === 0 || hotshotcrew === 0 || smokejumper === 0) {
+                        this.scene.pause('MapScene');
+                        dialog.setVisible(true);
+                        text.setVisible(true);
+                        if (toggle === true) d_count += 1;
+                        toggle = false;
+                    }
                 }
             }
         });
